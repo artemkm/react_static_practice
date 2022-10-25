a980a9db6f83f77a93e260499264d2f8a957c757
Artem Khan-Magomedov <dagermeister@mail.ru>
2022-10-25 17:40:16 +0300

    started a simple markup from figma

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-ReactDOM.render(<h1>Hello, this is second test!</h1>, document.getElementById("root"))
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
